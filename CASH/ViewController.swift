8f1ba6e8b4e05e3ddfda959a6539b1d76fce4fe7
Hamza Ahmed <hamzasahmed11@gmail.com>
2022-02-28 09:57:24 -0500

    added collection table/updated files

--- CASH/ViewController.swift
+++ CASH/ViewController.swift
@@ -4,11 +4,22 @@
 //
 //  Created by stephen weber on 2/25/22.
 //
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    var textdata = ["Hotel Suite","Dining Room,", "Gym", "Spa Services","Scuba Diving"]
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return textdata.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        var cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! customCollectionViewCell
+        cell.lb.text = textdata[indexPath.row]
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ CASH/customCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  customCollectionViewCell.swift
+//  CASH
+//
+//  Created by admin on 2/28/22.
+//
+
+import UIKit
+
+class customCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var lb: UILabel!
+}
