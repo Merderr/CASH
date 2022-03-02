d771c6cc2f2c96b8ea51ec9af81ba863a5e767e9
stephen-a-weber <pecuniary@gmail.com>
2022-03-02 12:25:00 -0500

    Created dataBase with no conflicting names , email and password are attributes

---
+++ ClientsData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ClientsData+CoreDataClass.swift
+//  CASH
+//
+//  Created by stephen weber on 3/2/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ClientsData)
+public class ClientsData: NSManagedObject {
+
+}
